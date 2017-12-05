56db666e7d1b2fc8f4f33c44fa58b2df5c7e4e93
Arnau Tarragó <arnautarragollorach@gmail.com>
2017-12-05 10:14:33 +0100

    jeje

--- TestSocketBall/FrmMain.cs
+++ TestSocketBall/FrmMain.cs
@@ -19,14 +19,14 @@ namespace TestSocketBall
     public partial class FrmMain : Form
     {
         //Name owner
-        public String ownerName = "Jordi";
+        public String ownerName = "Akrakor";
 
         //Mi ip
         public String ipLocal = "";
 
         //Vecinos
-        public String ipLeft =  "192.168.3.30";
-        public String ipRight = "192.168.3.25";
+        public String ipLeft =  "192.168.3.41";
+        public String ipRight = "192.168.3.24";
 
 
         //Guarda el JSon de la pelota
